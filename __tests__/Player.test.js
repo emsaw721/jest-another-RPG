3023c39d1a25e26bd9c19f4682c0855232970837
Emily Hill <emhillmed2021@gmail.com>
2022-10-10 13:00:05 -0500

    added more code to Player files, made __mocks__ and created mock Potion.js, added const Potion to Player.js, Player.test.js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,9 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion'); 
+
+console.log(new Potion()); 
+
 const Player = require('../lib/Player');
 
 test('creates a player object', () => {
@@ -7,6 +13,9 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number)); 
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    ); 
 }); 
 
 // if you want to run a particular group of related test (AKA test suite), you can run modified npm run -
