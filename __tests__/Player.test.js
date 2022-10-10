7492ccfa55772d6eb044ede42aa6aa52644f6fc3
Emily Hill <emhillmed2021@gmail.com>
2022-10-10 13:17:31 -0500

    learned .prototype syntax

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -18,5 +18,24 @@ test('creates a player object', () => {
     ); 
 }); 
 
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave'); 
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health'); 
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility'); 
+}); 
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave'); 
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = []; 
+
+    expect(player.getInventory()).toEqual(false); 
+}); 
+
 // if you want to run a particular group of related test (AKA test suite), you can run modified npm run -
 // in this case npm run test Player 
