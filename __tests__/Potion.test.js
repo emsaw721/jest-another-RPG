046369d85e7b1a62de71153cd2d498e5266906e3
Emily Hill <emhillmed2021@gmail.com>
2022-10-10 12:01:51 -0500

    created potion functions and passed tests

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -6,6 +6,10 @@ const Potion = require('../lib/Potion.js');
 test('creates a health potion object', () => {
     const potion = new Potion('health');
 
-    expect(potion.name).toBe('health');
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0); 
     expect(potion.value).toEqual(expect.any(Number)); 
 }); 
+
+// Do not use arrow functions as contructor functions, change the meaning of keyword "this",
+// which is a core piece of constructor functions. 
